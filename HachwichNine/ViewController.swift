221e4e034fb4b91b6efaa04bac9d690baf374384
Rhysa <rhysalee@hawaii.edu>
2020-04-09 12:52:30 -1000

    completed parts 1-5

--- HachwichNine/ViewController.swift
+++ HachwichNine/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.myLabel.text = ""
     }
 
-
+    @IBAction func segmentedControlPressed(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            self.myLabel.text = "Index One selected on the Segment Control"
+        case 1:
+            self.myLabel.text = "Index Teo selected, I got this"
+        default :
+            break
+        }
+    }
+    
 }
 
